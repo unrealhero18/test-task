a0e26f543064ac6254654005ffa303ccc5f39e64
Filimonov Kostya <slayer64@yandex.ru>
2017-01-14 17:06:34 +0200

    made header-top

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+
+  (function() {
+    $(".js-chosen-select").chosen({
+      disable_search: true,
+      inherit_select_classes: true
+    });
+  })();
+  
+});
